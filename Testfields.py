426cee5701072a929851704a230330b8d0cbe2aa
Alvis <alvislin43@gmail.com>
2019-11-09 19:07:39 +0000

    Import function in the testfields to run this programme in terminal

--- Testfields.py
+++ Testfields.py
@@ -2,7 +2,7 @@ import matplotlib
 from plotter import Plotter
 from io_file import IO_file
 from categoriser import Categoriser
-
+import sys
 matplotlib.use('TkAgg')
 
 
@@ -12,14 +12,13 @@ def main():
     polygon_points, x_polygon, y_polygon, polygon_lines = io.input_polyfile("polygon.csv")
 
     print("read input.csv")
-    input_points = io.input_pointfile("input.csv")
+    input_points = io.input_pointfile("input_test.csv")
 
     print("categorize points")
     cate = Categoriser()
     out_dots, idk_dots = cate.mbr_check(input_points, x_polygon, y_polygon) #MBR
     bou_dots = cate.boundary_check(idk_dots, polygon_lines) # POL
     idk_dots_2 = {k: v for k, v in idk_dots.items() if k not in bou_dots} # get rest points to classify after POL
-    print(idk_dots_2)
     in_dots, out_dots = cate.rca_check(idk_dots_2, polygon_points, out_dots) #RCA
     classified_points = {**out_dots, **bou_dots, **in_dots}
 
@@ -31,6 +30,9 @@ def main():
     plotter.add_polygon(x_polygon, y_polygon)  # plot Polygon
     plotter.add_all_point(classified_points.items()) # plot results
     plotter.show()
+
+    print(sys.argv) # run this programme in terminal
+
 
 if __name__ == "__main__":
     main()
--- plotter.py
+++ plotter.py
@@ -14,14 +14,16 @@ class Plotter:
     def add_polygon(self, xs, ys):
         plt.fill(xs, ys, 'lightblue', label='Polygon')
 
+
     def add_line(self, x, y):
         plt.plot(x, y, 'orange')
 
-    def add_ray(self,res):
+
+    def add_ray(self,res,x_max):
         for key, value in res:
             x = key.get_x()
             y = key.get_y()
-            plt.axhline(y,x, 20)
+            plt.axhline(y,x, x_max)
 
 
     def add_all_point(self,res):
@@ -37,6 +39,7 @@ class Plotter:
                 plt.plot(x, y, "go", label='Inside')
             else:
                 plt.plot(x, y, "ko", label='Unclassified')
+
 
     def add_point(self, x, y, kind=None):
         if kind == "outside":
